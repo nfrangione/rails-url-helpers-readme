8d8ee79aee690c03803c19e51960678188733f3c
Nicholas V Frangione <nfrangione8@gmail.com>
2021-04-27 16:27:42 -0700

    commit tree 3cabd684f3c1ecfb3554c38cef5ee9f87fbf7090

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "React", description: "Why we love it")
+Post.create(title: "Ruby", description: "Alrighty")
